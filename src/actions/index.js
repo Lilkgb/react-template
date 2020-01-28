e1cec68c6dd30518308efb9e6dd9d7b88803d4ab
Lilkgb <mcleanrs@comcast.net>
2020-01-27 20:17:32 -0800

    remote /app from line 2 in actions/index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,5 @@
 import constants from './../constants';
-// import * as firebase from 'firebase/app';
+// import * as firebase from 'firebase';
 const {types, firebaseConfig} = constants;
 
 // firebase.initializeApp(firebaseConfig);
